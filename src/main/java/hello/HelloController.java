7accbdbfa34a1c33ad19fb1a8876bd2812aa39ea
taylor <taylorcassidy64@gmail.com>
2020-02-26 19:03:41 -0500

    added ml stuff. doesn't work because of some logback issue i think, maybe conflicts with springboot?

--- src/main/java/hello/HelloController.java
+++ src/main/java/hello/HelloController.java
@@ -8,13 +8,11 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class HelloController {
     public static final Logger log = Logger.getLogger(HelloController.class.getName());
     
-    
     @RequestMapping("/")
     public String index() {
     	log.info("Inside hello controller...");
-        return "Greetings from Spring Boot!";
+    	String test = "The movie was terrible, i would not recommend it to my worst enemy";
+    	String res = CNNModel.doInference(test);
+        return "inference done!";
     }
-    
-    
-    
 }
