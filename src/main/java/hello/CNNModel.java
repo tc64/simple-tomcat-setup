3a8bde6f0b30f0b25272e479e9024b8401f1fdd9
taylor cassidy <taylorcassidy64@gmail.com>
2020-03-05 11:13:03 -0800

    update for test based on recommendations

--- src/main/java/hello/CNNModel.java
+++ src/main/java/hello/CNNModel.java
@@ -20,12 +20,19 @@ import org.apache.commons.io.FilenameUtils;
 import org.deeplearning4j.models.embeddings.loader.WordVectorSerializer;
 import org.deeplearning4j.models.embeddings.wordvectors.WordVectors;
 import org.deeplearning4j.nn.graph.ComputationGraph;
+import org.nd4j.linalg.api.memory.MemoryWorkspace;
+import org.nd4j.linalg.api.memory.conf.WorkspaceConfiguration;
+import org.nd4j.linalg.api.memory.enums.AllocationPolicy;
+import org.nd4j.linalg.api.memory.enums.LearningPolicy;
 import org.nd4j.linalg.api.ndarray.INDArray;
 import org.nd4j.linalg.factory.Nd4j;
 import org.nd4j.linalg.indexing.INDArrayIndex;
 import org.nd4j.linalg.indexing.NDArrayIndex;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import org.deeplearning4j.parallelism.ParallelInference;
+import org.deeplearning4j.parallelism.inference.InferenceMode;
 
 import java.io.File;
 import java.io.IOException;
@@ -51,6 +58,9 @@ public class CNNModel {
     private static ComputationGraph net;
     //private static INDArray[] out;
     //private static INDArray input;
+    
+    private static ParallelInference pi;
+    
     private static File f = new File(FilenameUtils.concat(System.getProperty("java.io.tmpdir"), "sentiment_cnn.model"));
     static {
     	System.out.println("static loading vectors");
@@ -62,7 +72,25 @@ public class CNNModel {
 			e.printStackTrace();
 		}
     	turnOffPeriodicGc();
+    	pi = new ParallelInference.Builder(net)
+                // BATCHED mode is kind of optimization: if number of incoming requests is too high - PI will be batching individual queries into single batch. If number of requests will be low - queries will be processed without batching
+                .inferenceMode(InferenceMode.BATCHED)
+
+                // max size of batch for BATCHED mode. you should set this value with respect to your environment (i.e. gpu memory amounts)
+                .batchLimit(32)
+
+                // set this value to number of available computational devices, either CPUs or GPUs
+                .workers(2)
+
+                .build();
+
     }
+    
+    private static WorkspaceConfiguration initialConfig = WorkspaceConfiguration.builder()
+            .initialSize(10 * 1024L * 1024L)
+            .policyAllocation(AllocationPolicy.STRICT)
+            .policyLearning(LearningPolicy.NONE)
+            .build();
     
     private static INDArray getVector(String word) {
         INDArray vector;
@@ -149,9 +177,10 @@ public class CNNModel {
     		logger.info("before getting input");
         	logBytesInfo();
     	}
-    	try (@SuppressWarnings("unchecked")
-		PointerScope scope = new PointerScope()) {
-    		INDArray[] out;
+    	//try (@SuppressWarnings("unchecked")
+		//PointerScope scope = new PointerScope()) {
+    	try(MemoryWorkspace ws = Nd4j.getWorkspaceManager().getAndActivateWorkspace(initialConfig, "SOME_ID")) {
+    		INDArray out;
     	    INDArray input;
     		input = dsFromString(txtToClassify);
     	
@@ -159,35 +188,19 @@ public class CNNModel {
 		    	logger.info("after getting input");
 		    	logBytesInfo();
 	    	}
-	    	out = CNNModel.net.output(input);
-    	}
-    	
-	    if (numServed % 10 == 0) 
-	    Pointer.attemptClearMemory(false, true, 1, 1);
-	    if (numServed % 1000 == 0) Pointer.attemptClearMemory(true, true, 10, 1);
+	    	out = CNNModel.pi.output(input);
+        }
+    		
+	    //if (numServed % 10 == 0) 
+	    //Pointer.attemptClearMemory(false, true, 1, 1);
+	    //if (numServed % 1000 == 0) Pointer.attemptClearMemory(true, true, 10, 1);
 	    	
     	if (numServed % 1000 == 0) {
 	    	logger.info("after output");
 	    	logBytesInfo();
+	    	System.gc();
+	    	logger.info("after system.gc");
     	}
-    	
-//    	Iterator<PointerScope> it = PointerScope.getScopeIterator();
-//        if (it != null) {
-//            while (it.hasNext()) {
-//                try {
-//                    it.next().deallocate();
-//                } catch (Exception e) {
-//                    // try the next scope down the stack
-//                    continue;
-//                }
-//                //break;
-//            }
-//        }
-        
-        if (numServed % 1000 == 0) {
-	        logger.info("after pointer scope dealloc");
-	    	logBytesInfo();
-        }
     	
     	numServed += 1;
     	
@@ -227,11 +240,11 @@ public class CNNModel {
     }
     
     public static void cleanDefault() {
-    	Pointer.attemptClearMemory(true, true, 1, 1);
+    	//Pointer.attemptClearMemory(true, true, 1, 1);
     }
 
     public static void cleanTrimOnly() {
-    	Pointer.attemptClearMemory(false, true, 1, 1);
+    	//Pointer.attemptClearMemory(false, true, 1, 1);
     }
     
     public static void main(String[] args) {
